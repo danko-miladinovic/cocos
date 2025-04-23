61042ad5f25372009e4880059e6b44cdc7d9cdb9
Ubuntu <azureuser@UVCTestCVM.bu0p0zdolasezg1jifpyqhaxuc.dx.internal.cloudapp.net>
2025-04-23 15:34:44 +0000

    fix linter errors

--- pkg/attestation/vtpm/vtpm.go
+++ pkg/attestation/vtpm/vtpm.go
@@ -112,7 +112,8 @@ func New(teeNonce, vtpmNonce, pubKeyTLS []byte, teeAttestation bool) attestation
 		TeeNonce:      teeNonce,
 		VTpmNonce:     vtpmNonce,
 		PubKeyTLS:     pubKeyTLS,
-		TeeAttestaion: teeAttestation}
+		TeeAttestaion: teeAttestation,
+	}
 }
 
 func (v provider) FetchAttestation() ([]byte, error) {
